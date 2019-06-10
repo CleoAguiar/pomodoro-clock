405a6a8c33c508433783c0f9f6a98d1d5aa712b4
Cleo Aguiar <cleotaisas@gmail.com>
2019-06-10 10:25:21 -0300

    feat: add default displays a value. User Story #5 and #6

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ class TimerLengthControl extends React.Component
     return e('div', {class: 'length-control'}, 
             [e('div', {id: this.props.titleID}, this.props.title),
              e('button', {id: this.props.dec, oncliCk: this.props.oncliCk}, '-'),
+             e('div', {id: this.props.lengthID}, this.props.length),
              e('button', {id: this.props.inc, oncliCk: this.props.oncliCk}, '+')]);
   }
 };
@@ -32,11 +33,20 @@ class TimerLengthControl extends React.Component
 
 class App extends React.Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+    }
+  }
+
 	render()
 	{
 		return [e(Header), 
-            e(TimerLengthControl, {titleID: 'break-label', title: 'Break Length', dec: 'break-decrement', inc: 'break-increment'}),
-            e(TimerLengthControl, {titleID: 'session-label', title: 'Session Length', dec: 'session-decrement', inc: 'session-increment'}),
+            e(TimerLengthControl, {titleID: 'break-label', title: 'Break Length', dec: 'break-decrement', inc: 'break-increment', lengthID: 'break-length' , length: this.state.breakLength}),
+            e(TimerLengthControl, {titleID: 'session-label', title: 'Session Length', dec: 'session-decrement', inc: 'session-increment', lengthID: 'session-length', length: this.state.sessionLength}),
             e(Footer)];
 	}
 };
